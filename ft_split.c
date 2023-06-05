a66756d4f4f8cd4e8381b81b6367622c5ddf864c
FerPulin <fpulin-v@student.42madrid.com>
2023-06-05 21:16:26 +0200

    subida ultimos

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,20 @@
 /*   By: fpulin-v <fpulin-v@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 23:09:09 by fpulin-v          #+#    #+#             */
-/*   Updated: 2023/05/09 19:08:47 by fpulin-v         ###   ########.fr       */
+/*   Updated: 2023/06/04 21:15:31 by fpulin-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- /*
- Create an array of string by splitting the strin 's' given into substrings
+#include "libft.h"
+
+static int	count_words(char const *s, char c);
+
+/*Create an array of string by splitting the strin 's' given into substrings
  using the character "c" as delimer. The fuction uses Malloc to allocate the
  requited memory space. The array must end with a NULL pointer. And the
  function will return the resulting array or a Null if the memory booking
  failes.
  */
-#include "libft.h"
-
-static int	count_words(char const *s, char c);
-
 static int	count_words(char const *s, char c)
 {
 	int	words;
